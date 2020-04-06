93e297b7805f3aae83f4a86977cbda39dfccf727
Amarnath <sharewithamar@gmail.com>
2020-04-06 12:05:08 +0530

    reusable custom form validator

--- src/app/math-validators.ts
+++ src/app/math-validators.ts
@@ -2,13 +2,19 @@ import { AbstractControl } from "@angular/forms";
 
 export class MathValidators {
   //static methods cannot access instance variable
-  static addition(form: AbstractControl) {
+  static addition(target: string, sourceOne: string, sourceTwo: string) {
+    return (form: AbstractControl) => {
+      const sum = form.value[target];
+      const firstNumber = form.value[sourceOne];
+      const secondNumber = form.value[sourceTwo];
+
+      if (firstNumber + secondNumber === parseInt(sum)) {
+        return null;
+      }
+      return { addition: true };
+    };
+
     //return null - form is valid
     //return {} - if form is invalid and has errors
-    const { a, b, answer } = form.value;
-    if (a + b === parseInt(answer)) {
-      return null;
-    }
-    return { addition: true };
   }
 }
